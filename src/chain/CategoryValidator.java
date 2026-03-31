d0392c8c41de1ab7d1c8336c1ddc91df7e14a052
hetparikh2003 <parikh.het@northeastern.edu>
2026-03-30 22:07:33 -0400

    Add CategoryValidator in validation chain

---
+++ src/chain/CategoryValidator.java
@@ -0,0 +1,30 @@
+package chain;
+
+// import db.DatabaseManager;
+import model.transaction.Transaction;
+
+public class CategoryValidator extends ValidationHandler {
+    @Override
+    public boolean validate(Transaction transaction) {
+        if (transaction == null || transaction.getCategoryId() <= 0) {
+            return false;
+        }
+
+        // String sql = "SELECT 1 FROM category WHERE id = ?";
+        // try {
+        //     Connection conn = DatabaseManager.getInstance().getConnection();
+        //     try (PreparedStatement ps = conn.prepareStatement(sql)) {
+        //         ps.setLong(1, transaction.getCategoryId());
+        //         try (ResultSet rs = ps.executeQuery()) {
+        //             if (!rs.next()) {
+        //                 return false;
+        //             }
+        //         }
+        //     }
+        // } catch (SQLException e) {
+        //     return false;
+        // }
+
+        return super.validate(transaction);
+    }
+}
