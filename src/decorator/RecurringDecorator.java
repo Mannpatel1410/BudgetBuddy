5658c0ff49f06a704aedb17ade531ba86ff19aa0
hetparikh2003 <parikh.het@northeastern.edu>
2026-04-06 08:33:15 -0400

    Add recurring frequency description

--- src/decorator/RecurringDecorator.java
+++ src/decorator/RecurringDecorator.java
@@ -3,8 +3,25 @@ package decorator;
 import model.transaction.Transaction;
 
 public class RecurringDecorator extends TransactionDecorator {
-    public RecurringDecorator(Transaction wrapped) {
-        super(wrapped);
-        wrapped.setRecurring(true);
+    private String frequency;
+
+    public RecurringDecorator(Transaction transaction, String frequency) {
+        super(transaction);
+        this.frequency = frequency;
+        wrappedTransaction.setRecurring(true);
+    }
+
+    @Override
+    public String getDescription() {
+        return wrappedTransaction.getDescription() + " [Recurring: " + frequency + "]";
+    }
+
+    @Override
+    public boolean isRecurring() {
+        return true;
+    }
+
+    public String getFrequency() {
+        return frequency;
     }
 }
